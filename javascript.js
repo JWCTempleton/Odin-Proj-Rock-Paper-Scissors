650ec8fa4fb1c51e0d78eefc30edb6b03c093d36
Jacob Templeton <jwctempleton@gmail.com>
2022-02-26 17:21:01 -0800

    Add win condition logic

--- javascript.js
+++ javascript.js
@@ -2,11 +2,11 @@ function computerPlay() {
   let result = "";
   let compPick = Math.floor(Math.random() * 3) + 1;
   if (compPick === 1) {
-    result = "Rock";
+    result = "rock";
   } else if (compPick === 2) {
-    result = "Paper";
+    result = "paper";
   } else {
-    result = "Scissors";
+    result = "scissors";
   }
   return result;
 }
@@ -17,3 +17,23 @@ function playerSelection() {
   ).toLocaleLowerCase();
   return playerChoice;
 }
+
+function playRound(playerChoice, computerChoice) {
+  if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return `You win, ${playerChoice} beats ${computerChoice}!`;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "scissors") ||
+    (playerChoice === "scissors" && computerChoice === "rock")
+  ) {
+    return `Computer wins, ${computerChoice} beats ${playerChoice}!`;
+  } else {
+    return "Tie game!";
+  }
+}
+
+console.log(playRound(playerSelection(), computerPlay()));
